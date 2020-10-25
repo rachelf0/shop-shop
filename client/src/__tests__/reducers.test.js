d05ac73524072f52a5f54cbc83d2ad7157f500da
Rachel Fritz <rachelfritz0@gmail.com>
2020-10-25 08:11:57 -0500

    added functions to homepage

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,3 +1,6 @@
+// import reducer() function 
+import { reducer } from '../utils/reducers';
+
 // import our actions
 import {
     UPDATE_PRODUCTS,
@@ -17,7 +20,27 @@ import {
       type: UPDATE_PRODUCTS,
       products: [{}, {}]
     });
-  
+    
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
+  });
+  
+  test('UPDATE_CATEGORIES', () => {
+      let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+    
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+  });
+  
+  test('UPDATE_CURRENT_CATEGORY', () => {
+      let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    });
+    
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
   });
